ec328ea39437d3d18b2a1c1233748abb5e08c29f
Stanislav Rybka <stanislav-rybka>
2024-02-04 03:43:11 +0200

    Modified header bar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,21 @@
+import { useContext } from 'react';
+import { Context } from '../../index';
+import { observer } from 'mobx-react-lite';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 
-const Header = () => {
+const Header = observer(() => {
+  const { user } = useContext(Context);
+
   return (
     <Navbar data-bs-theme="dark" bg="dark" >
+
       <Container>
-        <Navbar.Brand href="#home">
+
+        <Navbar.Brand>
           <img alt=""
                src="./quiz-logo-white.png"
                width="50"
@@ -15,15 +24,33 @@ const Header = () => {
           />
         </Navbar.Brand>
 
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Stanislav Rybka</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
+        {!user.isAuthorized ?(
+          
+          <Nav className="ml-auto">
+
+            <Button variant="outline-light" onClick={() => { user.setIsAuthorized(true) }}>Login</Button>
+
+          </Nav>
+
+        ) : (
+          
+          <Nav className="ml-auto">
+
+            <Navbar.Text className="me-4">
+              Signed in as: <a href="#login">{user.nickname}</a>
+            </Navbar.Text>
+
+            <Button variant="outline-light" onClick={() => { user.setIsAuthorized(false) }}>Logout</Button>
+
+          </Nav>
+
+        )}
+
       </Container>
+
     </Navbar>
   );
-};
+});
 
 
 export default Header;

a234f3b0b83bdbe9fb58fb8e79803718eb9b2484
Stanislav Rybka <stanislav-rybka>
2024-02-04 04:06:07 +0200

    Added conditional rendering of buttons inside header bar

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,10 +5,20 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { GAME_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE } from '../../utils/constants';
 
 
 const Header = observer(() => {
   const { user } = useContext(Context);
+
+  // Using hook to navigate to pages
+  const navigate = useNavigate();
+
+  // Using hook to identify current location (path)
+  const currentLocation = useLocation();
+  const isAuthorizationPage = [LOGIN_ROUTE, REGISTRATION_ROUTE].includes(currentLocation.pathname);
+
 
   return (
     <Navbar data-bs-theme="dark" bg="dark" >
@@ -24,12 +34,17 @@ const Header = observer(() => {
           />
         </Navbar.Brand>
 
-        {!user.isAuthorized ?(
+
+        {isAuthorizationPage ? (
           
           <Nav className="ml-auto">
+            <Button variant="outline-light" onClick={() => { navigate(GAME_ROUTE) }}>Back to Game</Button>
+          </Nav>
 
-            <Button variant="outline-light" onClick={() => { user.setIsAuthorized(true) }}>Login</Button>
-
+        ) : !user.isAuthorized ? (
+          
+          <Nav className="ml-auto">
+            <Button variant="outline-light" onClick={() => { navigate(LOGIN_ROUTE) }}>Login</Button>
           </Nav>
 
         ) : (

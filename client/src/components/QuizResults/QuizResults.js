75eaead558862a3f6d1881611e2eded7ee0dbf31
Stanislav Rybka <stanislav-rybka>
2024-02-06 04:47:52 +0200

    Added new content to quiz results component

--- client/src/components/QuizResults/QuizResults.js
+++ client/src/components/QuizResults/QuizResults.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Context } from '../../index';
 import { observer } from 'mobx-react-lite';
 import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
 
 
 const QuizResults = observer(({ onStartGame }) => {
@@ -9,20 +11,39 @@ const QuizResults = observer(({ onStartGame }) => {
   const { quiz } = useContext(Context);
 
 
+  // Action handlers
+  const handleStartAgainButtonClick = () => {
+    window.location.reload(false);
+  }
+
+
   return (
     <Card>
       <Card.Header className="text-center">
-        Results
+        <h4>Quiz Results</h4>
       </Card.Header>
 
       <Card.Body className="px-4">
-        <Card.Text className="mt-3 text-success">
-          Questions with correct answers: {quiz.successQuestions.length}
+        <Card.Text className="">
+          Congratulations! You have completed the "<b><i>{quiz.name}</i></b>" quiz with the following results:
         </Card.Text>
 
-        <Card.Text className="mt-4 text-danger">
-          Questions with wrong answers: {quiz.failedQuestions.length}
+        <Card.Text>
+          <ul>
+            <li className="text-success">
+              Correct answers: {quiz.successQuestions.length}
+            </li>
+            <li className="text-danger">
+              Wrong answers: {quiz.failedQuestions.length}
+            </li>
+          </ul>
         </Card.Text>
+
+        <Row className="mt-4">
+          <Button variant="outline-secondary" onClick={handleStartAgainButtonClick}>
+            Start Again
+          </Button>
+        </Row>
       </Card.Body>
     </Card>
   );
